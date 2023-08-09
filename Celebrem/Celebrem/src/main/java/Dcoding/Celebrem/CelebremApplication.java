877f48626ce247636e72ad8c58888f901a2cbd07
wonoong <dnd5301@gmail.com>
2023-08-09 15:07:59 +0900

    Initial Commit

---
+++ Celebrem/Celebrem/src/main/java/Dcoding/Celebrem/CelebremApplication.java
@@ -0,0 +1,13 @@
+package Dcoding.Celebrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebremApplication.class, args);
+	}
+
+}
---
+++ Celebrem/Celebrem/src/test/java/Dcoding/Celebrem/CelebremApplicationTests.java
@@ -0,0 +1,13 @@
+package Dcoding.Celebrem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
