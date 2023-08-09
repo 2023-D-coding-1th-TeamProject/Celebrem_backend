804b5369774d4eb0e763dfb6030e20e0485801d3
wonoong <dnd5301@gmail.com>
2023-08-09 15:32:03 +0900

    FEAT: Add tokenProvider, Member Entity

---
+++ Celebrem/Celebrem/src/main/java/Dcoding/Celebrem/domain/Member.java
@@ -0,0 +1,49 @@
+package Dcoding.Celebrem.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "member")
+public class Member {
+    @Id
+    @Column(name ="member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public Member(String email, String password, String name, Authority authority){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.authority = authority;
+    }
+
+    public static User memberToUser(Member member){
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.authority.toString());
+        return new User(
+                String.valueOf(member.id),
+                member.password,
+                Collections.singleton(grantedAuthority)
+        );
+    }
+}
