d012fb7d2585826fd199a97a5bc05439e5d09639
이정우 <acg6138@naver.com>
2023-08-17 11:10:46 +0900

    FEAT: Repository 메서드 추가 구현

--- src/main/java/Dcoding/Celebrem/repository/MemberRepository.java
+++ src/main/java/Dcoding/Celebrem/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package Dcoding.Celebrem.repository;
 
 import Dcoding.Celebrem.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
     Boolean existsMemberByEmail(String email);
     Boolean existsMemberByNickname(String nickname);
+
+    @Query("SELECT m FROM Member m JOIN FETCH m.profile p " +
+            "WHERE m.email = :email")
+    Optional<Member> findByEmailFetchProfile(@Param("email") String email);
 }
--- src/main/java/Dcoding/Celebrem/repository/ProfileTagRepository.java
+++ src/main/java/Dcoding/Celebrem/repository/ProfileTagRepository.java
@@ -1,7 +1,12 @@
 package Dcoding.Celebrem.repository;
 
 import Dcoding.Celebrem.domain.tag.ProfileTag;
+import Dcoding.Celebrem.domain.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileTagRepository extends JpaRepository<ProfileTag, Long> {
+
+    Optional<ProfileTag> findByTag(Tag tag);
 }
